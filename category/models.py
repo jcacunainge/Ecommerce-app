b7a27b8fccd4a78b348b9d860b766fc3c82ba9a4
Jhan Carlos Acuña Ordoñez <102251412+jcacunainge@users.noreply.github.com>
2023-07-11 19:07:49 -0500

    creación de app accounts

--- category/models.py
+++ category/models.py
@@ -8,6 +8,10 @@ class Category(models.Model):
     slug = models.CharField(max_length=100, unique=True)
     cat_image = models.ImageField(upload_to='photo/categories', blank = True)
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.category_name
 
